8a028d2c9c35ad188e745612928134ea2b72af70
Davi Marques Gonçalves <davisigon@gmail.com>
2022-01-19 14:11:14 -0300

    function

---
+++ src/function/multi.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+
+export const BoaTarde = props => <h1>Boa Tarde, {props.nome}! Bão?</h1>
+export const BoaNoite = props => <h1>Boa Noite {props.nome}! Ta na hora de durmi!</h1>
